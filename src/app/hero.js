516bab52a9d0c507affeb25289bee285857a3794
Timeiswax <58233833+Timeiswax@users.noreply.github.com>
2024-05-06 19:05:49 -0400

    Pretravel commit

--- src/app/hero.js
+++ src/app/hero.js
@@ -1,17 +1,38 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram, faSpotify,faBandcamp, faYoutube  } from "@fortawesome/free-brands-svg-icons";
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+const randomInt = getRandomInt(100)
+var tagline
+
+if (randomInt > 99) {
+  tagline = "suck my nuts covet"
+} else {
+  tagline = "perpetuity"
+}
+
 function Hero() {
   return(
       <div className="flex min-h-screen w-screen flex-col items-center justify-center">
-          <h1 className="text-5xl pb-3 h">bivouac cabs</h1>
-          <h2 className="text-xl">perpetuity</h2>
+          <h1 className="text-5xl pb-3">bivouac cabs</h1>
+          <h2 className="text-xl">{tagline}</h2>
           <h2 className="text-xl">5.31.24</h2>
-          <div className='w-screen text-center text-xl p-3'>
-          <FontAwesomeIcon icon={faInstagram} className='hover:opacity-50 px-3'></FontAwesomeIcon>
-          <FontAwesomeIcon icon={faSpotify} className='hover:opacity-50 px-3'></FontAwesomeIcon>
-          <FontAwesomeIcon icon={faBandcamp} className='hover:opacity-50 px-3'></FontAwesomeIcon>
-          <FontAwesomeIcon icon={faYoutube} className='hover:opacity-50 px-3'></FontAwesomeIcon>
+          <div className='w-screen text-center text-xl p-3'>         
+            <a href="https://open.spotify.com/artist/3abdFiVWboEJFU0yuezzzm?si=JWOkjunGRrm2C-nCZezSrQ" >
+              <FontAwesomeIcon icon={faSpotify} className='hover:blur-sm transition-all px-3' />
+            </a>  
+            <a href="https://open.spotify.com/artist/3abdFiVWboEJFU0yuezzzm?si=JWOkjunGRrm2C-nCZezSrQ" >
+              <FontAwesomeIcon icon={faInstagram} className='hover:blur-sm transition-all px-3' />
+            </a>  
+            <a href="https://open.spotify.com/artist/3abdFiVWboEJFU0yuezzzm?si=JWOkjunGRrm2C-nCZezSrQ" >
+              <FontAwesomeIcon icon={faBandcamp} className='hover:blur-sm transition-all px-3' />
+            </a>  
+            <a href="https://open.spotify.com/artist/3abdFiVWboEJFU0yuezzzm?si=JWOkjunGRrm2C-nCZezSrQ" >
+              <FontAwesomeIcon icon={faYoutube} className='hover:blur-sm transition-all px-3' />
+            </a>  
           </div>
     </div>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -8,9 +8,18 @@ const Hero = dynamic(() => import('../app/hero'), {ssr: false})
 export default function Home() {
   return (
     <div>
-      <Image src={'/img/bg.png'} alt="bg" fill="true" className='-z-10'></Image>
-      <div className="h-screen snap-y snap-mandatory">
+      <Image src={'/img/bg.png'} alt="bg" fill="true" className='-z-10 '></Image>
+      <div className='z-10 flex w-screen h-8 justify-around py-4 text-lg fixed'>
+        <a href="www.google.com" classname="hover:blur-sm transition-all">listen</a>
+        <a href="www.google.com" classname="hover:blur-sm transition-all">merch</a>
+        <a href="www.google.com" classname="hover:blur-sm transition-all">shows</a>
+        <a href="www.google.com" classname="hover:blur-sm transition-all">contact</a>
+      </div>
+      <div className="h-screen snap-y snap-mandatory overflow-y-scroll z-0">
         <Hero />
+        {/* SHOWS? */}
+        {/* PHOTOS? */}
+        {/* SHOWS? */}
       </div>
     </div>
 
